9f4dfea15629a2ba744873136cb561836c46d2c6
Kappa <g3kappa@gmail.com>
2015-07-23 01:06:08 +0200

    Updated test script to include callback testing

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,3 +1,9 @@
+FriendChatMsg_t = 805
+
+function onFriendChatMsg(cubParam, pubParam)
+	spp.print("You received or sent a message!\n");
+end
+
 function main(argc, argv)
 	spp.print("Hello from " .. argv[0] .. "!\nThis script tests every function that SPP binds to the LUA environment.\n\n")
 
@@ -11,9 +17,28 @@ function main(argc, argv)
 	for i = 0, argc - 1 do
 		spp.print("\t" .. argv[i] .. "\n")
 	end
-	spp.print("\n");
+	spp.print("\n")
 
-	
+	nTestsFailed = 0
 
-	return 0
+	spp.print("[TEST] Registering a callback.\nExpected return value: 0\n")
+	ret = spp.registerCallback(FriendChatMsg_t, "onFriendChatMsg")
+	spp.info("Returned value: " .. ret .. "\n")
+	if ret ~= 0 then 
+		spp.err("Unexpected return value.\n")
+		nTestsFailed = nTestsFailed + 1
+	end
+	--[[
+	spp.print("[TEST] Unregistering a callback.\nExpected return value: 0\n")
+	ret = spp.unregisterCallback(705)
+	spp.info("Returned value: " .. ret .. "\n")
+	if ret ~= 0 then 
+		spp.err("Unexpected return value.\n")
+		nTestsFailed = nTestsFailed + 1
+	end
+	]]--
+
+	spp.print("\n")
+
+	return nTestsFailed
 end
